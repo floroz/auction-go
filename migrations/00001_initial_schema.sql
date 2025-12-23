8a2434ca4fc72924b053d7c9ac7fbd84bc49756a
Daniele Tortora <45793869+floroz@users.noreply.github.com>
2025-12-23 10:41:48 +0100

    chore: initial schema to test migrations

---
+++ migrations/00001_initial_schema.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+CREATE TABLE items (
+    id UUID PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    start_price BIGINT NOT NULL,
+    current_highest_bid BIGINT DEFAULT 0,
+    end_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE bids (
+    id UUID PRIMARY KEY,
+    item_id UUID NOT NULL REFERENCES items(id),
+    user_id UUID NOT NULL,
+    amount BIGINT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE bids;
+DROP TABLE items;
+
